aa4278d50b0158fbbccd07a3a2e1447a6312c658
Аркадий <arkanio18@gmail.com>
2023-10-22 21:23:35 +0400

    Добавляет задание 5.16 (module5-task2)

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,24 @@
+const MINUTES_IN_HOUR = 60;
+
+const getMinutes = (time) => {
+  const[hour, minutes] = time.split(':').map(Number);
+  return hour * MINUTES_IN_HOUR + minutes;
+};
+
+const countTime = (startDay, endDay, startMeeting, duration) => {
+  const startDayMinutes = getMinutes(startDay);
+  const endDayMinutes = getMinutes(endDay);
+  const meetingMinutes = getMinutes(startMeeting);
+
+  const durationMeeting = meetingMinutes + duration <= endDayMinutes;
+  const startOfMeeting = meetingMinutes >= startDayMinutes;
+
+  return durationMeeting && startOfMeeting;
+};
+
+countTime('8:00', '17:30', '14:00', 220);
+
+/*
 function isLengthString(string, length) {
   return string.length <= length;
 }
@@ -40,3 +61,5 @@ extractNumber('я томат');
 extractNumber(2023);
 extractNumber(-1);
 extractNumber(1.5);
+
+*/
