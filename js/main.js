e5e6083242b0e301e2e281ec6275641737de1258
Аркадий <arkanio18@gmail.com>
2023-10-29 20:41:02 +0400

    Отображение миниатюр

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {getPictures} from './data.js';
+import {getPictures} from './mock.js';
+import {renderThumbnails} from './thumbnails.js';
 
-
-getPictures();
+renderThumbnails(getPictures());
