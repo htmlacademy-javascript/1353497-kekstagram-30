a66d1539dc46e6421c721a20a3377b396d7cbf35
Аркадий <arkanio18@gmail.com>
2023-11-06 17:05:53 +0400

    Делает открывается и закрывается (часть 1)

---
+++ js/gallery.js
@@ -0,0 +1,24 @@
+import { renderThumbnails } from './thumbnails.js';
+import { showPicture } from './big-picture.js';
+
+const container = document.querySelector('.pictures');
+
+// Обработчик кликов
+const renderGallery = (pictures) => { // Принимает массив с данными
+  container.addEventListener('click', (evt) => { // Отслеживаем события в контейнере, в evt подставляется объект события
+    const thumbnail = evt.target.closest('[data-thumbnail-id]'); // Проверка у элемента наличие дата атрибута, closest ищет ближайший родительский элемент
+
+    if (! thumbnail) {
+      return; // Если не находит, то выход
+    }
+
+    evt.preventDefault();
+    const thumbnailId = +thumbnail.dataset.thumbnailId; // Получаем значение атрибута из разметки, + это перевод строки в число
+    const pictureData = pictures.find(({id}) => id === thumbnailId); // Находим id в контейнере, который соответствует thumbnailId. Объект, по которому кликнули
+
+    showPicture(pictureData); // Функция показа полного изображния
+  });
+
+  renderThumbnails(pictures, container);
+};
+export { renderGallery };
